6dbe2ba72fb9877df9409f3961ce0d14cebdf602
abhirajthakur <abhirajthakur124@gmail.com>
2025-08-12 22:29:43 +0530

    add noice.lua

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,21 +1,17 @@
 return {
   { "folke/flash.nvim", enabled = false },
 
-  {
-    "stevearc/conform.nvim",
-    opts = function()
-      local opts = {
-        formatters_by_ft = {
-          javascript = { "biome" },
-          javascriptreact = { "biome" },
-          typescript = { "biome" },
-          typescriptreact = { "biome" },
-        },
-        default_format_opts = {
-          lsp_format = "fallback",
-        },
-      }
-      return opts
-    end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   opts = { style = "night" },
+  -- },
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "tokyonight",
+  --   },
+  -- }
+
 }
