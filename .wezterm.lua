114e4699b5cf211552ac9c7f477c918928b7e066
Abhiraj Thakur <abhirajthakur124@gmail.com>
2025-11-06 20:03:57 +0530

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,60 +5,64 @@ local act = wezterm.action
 config.default_domain = "WSL:Ubuntu-22.04"
 config.audible_bell = "Disabled"
 
-config.font_size = 10.5
-config.font = wezterm.font("FiraCode Nerd Font", { weight = 'Medium' })
-config.color_scheme = "tokyonight_night"
--- config.color_scheme = "Apple System Colors"
+config.font_size = 11
+config.font = wezterm.font_with_fallback({
+  { family = "FiraCode Nerd Font" },
+  "JetBrains Mono",
+  "Noto Color Emoji",
+  "Symbols Nerd Font Mono",
+})
+
+-- config.color_scheme = "tokyonight_night"
+config.color_scheme = "Apple System Colors"
 config.colors = {
-    cursor_bg = "#7aa2f7",
-    cursor_border = "#7aa2f7"
+  cursor_bg = "#7aa2f7",
+  cursor_border = "#7aa2f7",
 }
 
-config.mouse_bindings = mouse_bindings
-config.keys = {
-    {
-        key = "V",
-        mods = "CTRL",
-        action = act.PasteFrom "Clipboard"
-    },
-    {
-        key = "w",
-        mods = "CTRL",
-        action = act.CloseCurrentPane {confirm = true}
-    },
-    {
-        key = "d",
-        mods = "CTRL",
-        action = act.SplitHorizontal {domain = "CurrentPaneDomain"}
-    },
-    {
-        key = "e",
-        mods = "CTRL",
-        action = act.SplitVertical {domain = "CurrentPaneDomain"}
-    },
-    {
-        key = "Backspace",
-        mods = "CTRL",
-        action = act.SendKey {key = "w", mods = "CTRL"}
-    },
-    {
-        key = "Delete",
-        mods = "CTRL",
-        action = act.SendKey {key = "W", mods = "CTRL"}
-    },
-    {
-        key = "LeftArrow",
-        mods = "CTRL",
-        action = act.SendKey {key = "b", mods = "ALT"}
-    },
-    {
-        key = "RightArrow",
-        mods = "CTRL",
-        action = act.SendKey {key = "f", mods = "ALT"}
-    }
-}
-
--- config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE" c
 config.enable_tab_bar = false
+config.mouse_bindings = mouse_bindings
+
+config.keys = {
+  {
+    key = "v",
+    mods = "CTRL",
+    action = act.PasteFrom "Clipboard",
+  },
+  {
+    key = "w",
+    mods = "CTRL",
+    action = act.CloseCurrentPane { confirm = true },
+  },
+  { key = "d", 
+    mods = "CTRL", 
+    action = act.SplitHorizontal {domain = "CurrentPaneDomain"} 
+  }, 
+  { key = "e", 
+    mods = "CTRL", 
+    action = act.SplitVertical {domain = "CurrentPaneDomain"} 
+  },
+  {
+    key = "Backspace",
+    mods = "CTRL",
+    action = act.SendKey { key = "w", mods = "CTRL" },
+  },
+  {
+    key = "Delete",
+    mods = "CTRL",
+    action = act.SendString("\x1bd"),
+  },
+  {
+    key = "LeftArrow",
+    mods = "CTRL",
+    action = act.SendKey { key = "b", mods = "ALT" },
+  },
+  {
+    key = "RightArrow",
+    mods = "CTRL",
+    action = act.SendKey { key = "f", mods = "ALT" },
+  },
+}
 
 return config
