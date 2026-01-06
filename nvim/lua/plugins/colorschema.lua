4de5dc24c5bb29d63cdfbed9e7331f9a2a3c377f
Abhiraj Thakur <abhirajthakur124@gmail.com>
2026-01-06 19:07:14 +0530

    Enhance tokyonight colorscheme with transparency options
    Added transparent background options for the tokyonight colorscheme.

--- nvim/lua/plugins/colorschema.lua
+++ nvim/lua/plugins/colorschema.lua
@@ -2,7 +2,11 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "night" },
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = { sidebars = "transparent", floats = "transparent" },
+    },
   },
 
   {
