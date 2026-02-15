eb220c9e1554a26b80abaaf8eebd25e50b47561c
Abhiraj Thakur <abhirajthakur124@gmail.com>
2026-02-15 23:29:10 +0530

    Disable inlay hints for nvim-lspconfig

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,6 +2,13 @@ return {
   { "folke/flash.nvim", enabled = false },
 
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+
+  {
     "folke/noice.nvim",
     opts = function(_, opts)
       table.insert(opts.routes, {
